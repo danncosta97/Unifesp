e0a5d8990df3e60f63826640e8df87fe4af44ade
danielcosta <danielbarbosa.0797@gmail.com>
2021-11-01 01:33:55 -0300

    [03] Remoção da função while() do código
    Realizado: Remoção da função bloqueante while() da versão [02].
    
    Problema: Aparente diminuição da velocidade de transmissão bem sucedida em comparação da versão [02] (CLOCK_PERIOD=4) para a versão [03] (CLOCK_PERIOD>=6).
    
    Próximos Passos: Substituir LDR pelo módulo TEMT6000 nas versões [02] e [03]. Verificar a possibilidade de aprimorar o analogRead() para aumento da transmissão de dados.

--- TCC/Receiver/Receiver.ino
+++ TCC/Receiver/Receiver.ino
@@ -1,56 +1,93 @@
 // MEGA PORT COM 4 (USB DIR)
 //--------------------------
 // Port direct A0 -> PORTF[0] 0000000$
+
 #define LDR A0 // A0
-#define CLK_PERIOD 500
 
-int baudDataBitIndex = 0;
+#define CLK_PERIOD 10
+
+#define IDLE_STATE 0 // Estado de aguardo do proximo Start Bit
+#define DELAY_STATE 10 // Estado de delay para amostragem otima
+#define START_BIT_STATE 1 // Estado de envio do Start Bit
+#define DATA_BITS_STATE 2 // Estado de envio dos Bits do char
+#define STOP_BIT_STATE 3 // Estado de envio do Stop Bit
+
+#define DEBUG 1 // Habilita e desabilita prints no Serial
+
 char charBits;
-
-long millis_current = 0;
-long millis_previous = 0;
-
 int ldrValue;
 int bitReceived;
 
+int state;
+
+int baudDataBitIndex;
+
+long millisPrevious = 0;
+
 void setup() {
+    // Habilita o Serial
     Serial.begin(9600);
-    // LED integrado
+    // Desliga o LED integrado
     pinMode(LED_BUILTIN, OUTPUT);
     digitalWrite(LED_BUILTIN, LOW);
 
     charBits = 0;
     ldrValue = 0;
     bitReceived = 0;
+
+    state = IDLE_STATE;
+
+    baudDataBitIndex = 0;
     
-    millis_current = 0;
-    millis_previous = 0;
+    millisPrevious = 0;
 }
 
 void loop() {
-    millis_previous = millis();
-    ldrValue = analogRead(LDR);
-    if(ldrValue < 900) {
-        // Amostragem ótima - centro do bit
-        while (millis() - millis_previous < CLK_PERIOD*0.5){
-            //
-        }millis_previous = millis();
-        Serial.print("<S");
-        while (millis() - millis_previous < CLK_PERIOD) {
-            //
-        }millis_previous = millis();
-        
-        // Bits de dados (LSB para MSB)
-        for (baudDataBitIndex=0; baudDataBitIndex<8; baudDataBitIndex++){
+
+    if(state == IDLE_STATE) {
+        if (analogRead(LDR) < 900) {
+            state = START_BIT_STATE;
+
+            millisPrevious = millis();
+        }
+    }
+
+    /*if(state == DELAY_STATE) {
+        if((millis() - millisPrevious) >= CLK_PERIOD*0.5) {
+            state = START_BIT_STATE;
+            millisPrevious = millis();
+        }
+    }*/
+
+    if(state == START_BIT_STATE) {
+        if((millis() - millisPrevious) >= CLK_PERIOD*0.5) {
+            Serial.print("<S");
+            state = DATA_BITS_STATE;
+            millisPrevious = millis();
+        }
+    }
+
+    if(state == DATA_BITS_STATE) {
+        if((millis() - millisPrevious) >= CLK_PERIOD) {
             bitReceived = analogRead(LDR) > 900 ? 1 : 0;
             Serial.print(bitReceived);
             charBits = charBits | (bitReceived << baudDataBitIndex);
-            while (millis() - millis_previous < CLK_PERIOD){
-                //
-            }millis_previous = millis();
+            baudDataBitIndex++;
+            if(baudDataBitIndex >= 8) {
+                baudDataBitIndex = 0;
+                state = STOP_BIT_STATE;
+            }
+            millisPrevious = millis();
         }
-        Serial.print("S>");
-        Serial.println(charBits);
-        charBits = 0;
+    }
+
+    if(state == STOP_BIT_STATE) {
+        if((millis() - millisPrevious) >= CLK_PERIOD) {
+            Serial.print("S>");
+            Serial.println(charBits);
+            charBits = 0;
+            state = IDLE_STATE;
+            millisPrevious = millis();
+        }
     }
 }
--- TCC/Transmitter/Transmitter.ino
+++ TCC/Transmitter/Transmitter.ino
@@ -1,76 +1,114 @@
 // NANO PORT COM 5 (USB ESQ)
 //--------------------------
 // Port direct D2 -> PORTD[2] 00000$00
-#define CLK_PERIOD 500
+
+#define CLK_PERIOD 10 // Precisa ser igual ao do receptor
+
+#define START_STATE 0 // Estado de inicializacao, tempo para iniciar o receptor
+#define START_BIT_STATE 1 // Estado de envio do Start Bit
+#define DATA_BITS_STATE 2 // Estado de envio dos Bits do char
+#define STOP_BIT_STATE 3 // Estado de envio do Stop Bit
+
+#define DEBUG 1 // Habilita e desabilita prints no Serial
 
 char* string = "This is a test transmission";
-int stringLength = 0;
-int baudDataBitIndex = 0;
+int stringLength;
+int stringLengthSent;
 
-unsigned long millis_current = 0;
-unsigned long millis_previous = 0;
+int state;
+
+int baudDataBitIndex;
+
+unsigned long millisPrevious;
 
 void setup() {
+    // Habilita o Serial
     Serial.begin(9600);
-    // LED integrado
+    // Desliga o LED integrado
     pinMode(LED_BUILTIN, OUTPUT);
     digitalWrite(LED_BUILTIN, LOW);
 
     // D7~D2.TX.RX
-    DDRD = DDRD | B00000100; // inputs e outputs;
+    DDRD = DDRD | B00000100; // inputs e outputs; D2 Output
     PORTD = B00000100; // valor alto ou baixo; D2 Alto
-    
-    stringLength = strlen(string);
 
-    long millis_in = 0;
-    long millis_out = 0;
+    stringLength = 0;
+    stringLength = strlen(string);
+    stringLengthSent = 0;
+
+    state = START_STATE;
+    baudDataBitIndex = 0;
+
+    millisPrevious = 0;
+    millisPrevious = millis();
 }
 
 void loop() {
-    PORTD = B00000100;
-  
-    //Start System
-    while (millis() - millis_previous < 5000) {
-    
-    }millis_previous = millis();
-      
-    for(int i = 0; i < stringLength; i ++) {
-        startBit();
-        
-        dataBits(string[i]);
-        
-        stopBit();
-    }
-  
-}
+    //PORTD = B00000100;
 
-void startBit() {
-    PORTD = B00000000;
-    Serial.print("<S");
-    while (millis() - millis_previous < CLK_PERIOD) {
-        //
-    }millis_previous = millis();
-}
-
-void dataBits(char dataByte){
-    for(baudDataBitIndex=0; baudDataBitIndex<8; baudDataBitIndex++) {
-        // Envio do LSB para o MSB
-        if((dataByte & (0x01 << baudDataBitIndex)) != 0) {
-            PORTD = B00000100;
-        } else {
-            PORTD = B00000000;
+    // Matém o LED aceso por 5s.
+    if(state == START_STATE) {
+        if((millis() - millisPrevious) >= 5000) {
+            state = START_BIT_STATE;
+            
+            millisPrevious = millis();
         }
-        Serial.print((dataByte & (0x01 << baudDataBitIndex)) != 0);
-        while (millis() - millis_previous < CLK_PERIOD) {
-            //
-        }millis_previous = millis();
     }
-}
 
-void stopBit() {
-    PORTD = B00000100;
-    Serial.println("S>");
-    while (millis() - millis_previous < CLK_PERIOD) {
-        //
-    }millis_previous = millis();
+    // Faz o envio do Start Bit (nível baixo).
+    if(state == START_BIT_STATE) {
+        if((millis() - millisPrevious) >= CLK_PERIOD) {
+            PORTD = B00000000;
+            state = DATA_BITS_STATE;
+            
+            if(DEBUG) Serial.print("<S");
+            
+            millisPrevious = millis();
+        }
+    }
+
+    // Faz o envio dos Bits do Char;
+    // Ao enviar 8 bits (baudDataBitIndex==8), vai para o Stop Bit
+    // e incrementa para o próximo caracter da string (stringLengthSent++).
+    if(state == DATA_BITS_STATE) {
+        if((millis() - millisPrevious) >= CLK_PERIOD) {
+            // Bits de dados (LSB para MSB)
+            if((string[stringLengthSent] & (0x01 << baudDataBitIndex)) != 0) {
+                PORTD = B00000100;
+            } else {
+                PORTD = B00000000;
+            }
+            
+            if(DEBUG) Serial.print((string[stringLengthSent] & (0x01 << baudDataBitIndex)) != 0);
+            
+            baudDataBitIndex++;
+            
+            if(baudDataBitIndex >= 8) {
+                state = STOP_BIT_STATE;
+                baudDataBitIndex = 0;
+                stringLengthSent++;
+            }
+            
+            millisPrevious = millis();
+        }
+    }
+
+    // Faz o envio do Stop Bit (nível alto) e volt para o Start Bit;
+    // Se a qtd de char enviados for maior ou igual ao tamanho da frase
+    // Volta para estado de inicialização do sistema.
+    if(state == STOP_BIT_STATE) {
+        if((millis() - millisPrevious) >= CLK_PERIOD) {
+            PORTD = B00000100;
+            state = START_BIT_STATE;
+            
+            if(DEBUG) Serial.println("S>");
+            
+            if(stringLengthSent >= stringLength){
+                    stringLengthSent = 0;
+                    state = START_STATE;
+            }
+            
+            millisPrevious = millis();
+        }
+    }
 }
