263dd92cdeffb90952a3e5f96f6e0b70e04abaac
danielcosta <danielbarbosa.0797@gmail.com>
2021-10-31 23:45:18 -0300

    [02] Comunicação LED e LDR funcional
    Comunicação estabelecida corretamente com a inserção do Stop Bit. Ainda há laços bloqueantes (while()) no código.
    
    Próximo Passo: remover while() ao longo do código.

--- TCC/Receiver/Receiver.ino
+++ TCC/Receiver/Receiver.ino
@@ -1,164 +1,56 @@
 // MEGA PORT COM 4 (USB DIR)
-#define LDR A0 // A0;
-#define START_STATE 0
-#define DATA_STATE 1
-#define PARITY_STATE 2
-#define COMM_STARTED_STATE 3
+//--------------------------
+// Port direct A0 -> PORTF[0] 0000000$
+#define LDR A0 // A0
+#define CLK_PERIOD 500
 
-int state = 0;
+int baudDataBitIndex = 0;
+char charBits;
 
-int baudDataPosition = 0;
+long millis_current = 0;
+long millis_previous = 0;
 
-int bitReceived = 0;
-
-int ldrValue = 0;
-
-char charBits = 0;
-
-unsigned long startCycle = 0;
-unsigned long clockCycle = 500; // ms
-unsigned long initialDelay = 750; // delay para amostrar no tempo central da bit
-
-unsigned long millis_in = 0;
-unsigned long millis_out = 0;
-
-unsigned long millis_current = 0;
-unsigned long millis_previous = 0;
-
-unsigned long outloop = 0;
-unsigned long inloop = 0;
-unsigned long betweenloop = 0;
-
-int start=0;
-
-//-----------------------
-//Port direct A0 -> PORTF[0]
+int ldrValue;
+int bitReceived;
 
 void setup() {
     Serial.begin(9600);
-    // led integrado
+    // LED integrado
     pinMode(LED_BUILTIN, OUTPUT);
     digitalWrite(LED_BUILTIN, LOW);
 
-    int state = START_STATE;
-
-    long millis_in = 0;
-    long millis_out = 0;
-
-    long millis_current = 0;
-    long millis_previous = 0;
+    charBits = 0;
+    ldrValue = 0;
+    bitReceived = 0;
+    
+    millis_current = 0;
+    millis_previous = 0;
 }
 
 void loop() {
     millis_previous = millis();
     ldrValue = analogRead(LDR);
-    
-    /*if(ldrValue < 900 && state == START_STATE) {
-        millis_in = millis();
-        delay(clockCycle*0.5);
-        Serial.print('S');
-        delay(clockCycle);
-        state = DATA_STATE;
-    }
-
-    if(state == DATA_STATE) {
+    if(ldrValue < 900) {
+        // Amostragem ótima - centro do bit
+        while (millis() - millis_previous < CLK_PERIOD*0.5){
+            //
+        }millis_previous = millis();
+        Serial.print("<S");
+        while (millis() - millis_previous < CLK_PERIOD) {
+            //
+        }millis_previous = millis();
         
-        Serial.print(bitReceived);
-        charBits = charBits | (bitReceived << baudDataPosition);
-        baudDataPosition++;
-        if (baudDataPosition > 7){
-            baudDataPosition = 0;
-            state = START_STATE;
-            Serial.print(charBits);
-            charBits = 0;
-            delay(clockCycle*0.5);
-            Serial.print("  ");
-            millis_out = millis();
-            Serial.println(millis_out - millis_in);
-        } else {
-            delay(clockCycle);
-        }
-    }*/
-
-    /*if(ldrValue < 900) {
-        millis_in = millis();
-        delay(clockCycle*0.5);
-        Serial.print('S');
-        delay(clockCycle);
-        for (int i=0; i<8; i++){
+        // Bits de dados (LSB para MSB)
+        for (baudDataBitIndex=0; baudDataBitIndex<8; baudDataBitIndex++){
             bitReceived = analogRead(LDR) > 900 ? 1 : 0;
             Serial.print(bitReceived);
-            charBits = charBits | (bitReceived << i);
-            if(i!=7) delay(clockCycle);
-            else delay(clockCycle*0.5);
+            charBits = charBits | (bitReceived << baudDataBitIndex);
+            while (millis() - millis_previous < CLK_PERIOD){
+                //
+            }millis_previous = millis();
         }
-        Serial.print(charBits);
+        Serial.print("S>");
+        Serial.println(charBits);
         charBits = 0;
-        Serial.print("  ");
-        millis_out = millis();
-        Serial.println(millis_out - millis_in);
-        //delay(clockCycle*0.5);
-    }*/
-    /*inloop = millis();
-    betweenloop += (inloop - outloop);
-    //Serial.println("");
-    if(ldrValue < 900) {
-        //Serial.println(betweenloop);
-        betweenloop=0;
-        if(start==0){
-           millis_previous = millis();
-           start = 1;
-        }
-        millis_in = millis();
-        millis_current = millis();
-        if (millis_current - millis_previous >= clockCycle*1.5){
-            millis_previous = millis_current;
-            Serial.print('S');
-            for (int i=0; i<8; i++){
-                bitReceived = analogRead(LDR) > 900 ? 1 : 0;
-                Serial.print(bitReceived);
-                charBits = charBits | (bitReceived << i);
-                if(i!=7) {
-                    while (millis() - millis_previous < clockCycle){
-                        
-                    }millis_previous = millis();
-                }
-                else {
-                    while (millis() - millis_previous < clockCycle*0.5){
-                        
-                    }millis_previous = millis();
-                }
-            }
-            Serial.print(charBits);
-            charBits = 0;
-            Serial.print("  ");
-            millis_out = millis();
-            Serial.println(millis_out - millis_in);
-            //delay(clockCycle*0.5);
-        }
-    outloop = millis();
-    }*/
-    if(ldrValue < 900) {
-        while (millis() - millis_previous < clockCycle/2){
-            //
-        }millis_previous = millis();
-        Serial.print('S');
-        while (millis() - millis_previous < clockCycle){
-            //
-        }millis_previous = millis();
-        for (int i=0; i<8; i++){
-            bitReceived = analogRead(LDR) > 900 ? 1 : 0;
-            Serial.print(bitReceived);
-            charBits = charBits | (bitReceived << i);
-            if(i!=7) {
-                while (millis() - millis_previous < clockCycle){
-                      //  
-                }millis_previous = millis();
-            } else { // ultimo bit lido
-                while (millis() - millis_previous < 250){
-                    //
-                }millis_previous = millis();
-            }
-        }
     }
 }
--- TCC/Transmitter/Transmitter.ino
+++ TCC/Transmitter/Transmitter.ino
@@ -1,70 +1,76 @@
 // NANO PORT COM 5 (USB ESQ)
-#define LED_PIN 2
-#define BUTTON_PIN A0
-#define PERIOD 500
+//--------------------------
+// Port direct D2 -> PORTD[2] 00000$00
+#define CLK_PERIOD 500
 
 char* string = "This is a test transmission";
-int string_length;
-
-long millis_in = 0;
-long millis_out = 0;
+int stringLength = 0;
+int baudDataBitIndex = 0;
 
 unsigned long millis_current = 0;
 unsigned long millis_previous = 0;
 
-void setup() 
-{
+void setup() {
     Serial.begin(9600);
-    //pinMode(LED_PIN, OUTPUT);
-    //pinMode(BUTTON_PIN, INPUT_PULLUP);
+    // LED integrado
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, LOW);
 
-    //D7~D2.TX.RX
+    // D7~D2.TX.RX
     DDRD = DDRD | B00000100; // inputs e outputs;
     PORTD = B00000100; // valor alto ou baixo; D2 Alto
     
-    string_length = strlen(string);
+    stringLength = strlen(string);
 
     long millis_in = 0;
     long millis_out = 0;
 }
 
-void loop() 
-{
-  PORTD = B00000100;
-  while (millis() - millis_previous < 5000){
-            //
-        }
-  for(int i = 0; i < string_length; i ++)
-  {
-    millis_in = millis();
-    send_byte(string[i]);
-    millis_out = millis();
-    Serial.print("  ");
-    Serial.println( millis_out - millis_in);
-  }
-  //delay(1000);
+void loop() {
+    PORTD = B00000100;
+  
+    //Start System
+    while (millis() - millis_previous < 5000) {
+    
+    }millis_previous = millis();
+      
+    for(int i = 0; i < stringLength; i ++) {
+        startBit();
+        
+        dataBits(string[i]);
+        
+        stopBit();
+    }
+  
 }
 
-void send_byte(char my_byte)
-{
+void startBit() {
     PORTD = B00000000;
-    Serial.print('S');
-    millis_previous = millis();
-    while (millis() - millis_previous < PERIOD){
+    Serial.print("<S");
+    while (millis() - millis_previous < CLK_PERIOD) {
         //
-    }
-    for(int i = 0; i < 8; i++)
-    {
-        if((my_byte&(0x01 << i))!=0){
+    }millis_previous = millis();
+}
+
+void dataBits(char dataByte){
+    for(baudDataBitIndex=0; baudDataBitIndex<8; baudDataBitIndex++) {
+        // Envio do LSB para o MSB
+        if((dataByte & (0x01 << baudDataBitIndex)) != 0) {
             PORTD = B00000100;
         } else {
             PORTD = B00000000;
         }
-        //digitalWrite(LED_PIN, (my_byte&(0x01 << i))!=0 );
-        Serial.print((my_byte&(0x01 << i))!=0);
-        millis_previous = millis();
-        while (millis() - millis_previous < PERIOD){
+        Serial.print((dataByte & (0x01 << baudDataBitIndex)) != 0);
+        while (millis() - millis_previous < CLK_PERIOD) {
             //
-        }
+        }millis_previous = millis();
     }
+}
+
+void stopBit() {
+    PORTD = B00000100;
+    Serial.println("S>");
+    while (millis() - millis_previous < CLK_PERIOD) {
+        //
+    }millis_previous = millis();
 }
